460853ed1a45ac9dbddd7452587aae2b804639b4
juneqing <juneqingl@gmail.com>
2017-08-10 11:09:39 +0800

    8.10点赞

--- js/js.js
+++ js/js.js
@@ -1,6 +1,61 @@
-// $.ajax({
-//     url:'http://wq8.youqulexiang.com/wechat/jssdk',
-//     success:function(rtn){
-//         console.log(rtn)
-//     }
-// })
+function getThumbsUp() {
+    $.ajax({
+        url: 'http://wq8.youqulexiang.com/share/thumbsup?url=' + location.href,
+        success: function(rtn) {
+            $('#number').text(rtn.data);
+        }
+    })
+
+
+}
+
+
+$(function() {
+    getThumbsUp();
+})
+
+
+function postThumbsUp() {
+    if (!isThumbsedPage(location.href)) {
+        storageThumbsUpPage(location.href);
+
+
+        $.ajax({
+            url: 'http://wq8.youqulexiang.com/share/thumbsup',
+            method: 'post',
+            data: { url: location.href },
+            success: function(rtn) {
+
+                getThumbsUp();
+
+            }
+        })
+    }
+}
+
+function storageThumbsUpPage(url) {
+    var pagesStr = localStorage.getItem('pages');
+    var pages = JSON.parse(pagesStr ? pageStr : '[]');
+
+
+    pages.push(url);
+    localStorage.setItem('pages', JSON.stringify(pages));
+}
+
+/**
+ * 是否已经点过赞
+ * @param {*} url 
+ */
+function isThumbsedPage(url) {
+    var pagesStr = localStorage.getItem('pages');
+    var pages = JSON.parse(pagesStr ? pagesStr : '[]');
+    let result = false;
+    pages.forEach(function(page) {
+        if (page == url) {
+            result = true
+        }
+    });
+    console.log(result);
+    // debugger;
+    return result;
+}
